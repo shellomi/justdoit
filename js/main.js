4af597472c9029dbaba6adf936fa32dc26e8ed0f
binson1989 <binson1989@gmail.com>
2015-12-26 13:50:41 +0530

    Styling close button for video #2

--- js/main.js
+++ js/main.js
@@ -15,4 +15,17 @@ function main() {
   });
 }
 
+// function autoPlayYouTubeModal() {
+//     var trigger = $("body").find('[data-toggle="modal"]');
+//     trigger.click(function () {
+//         var theModal = $(this).data("target"),
+//             videoSRC = $(this).attr("data-theVideo"),
+//             videoSRCauto = videoSRC + "?autoplay=1";
+//         $(theModal + ' iframe').attr('src', videoSRCauto);
+//         $(theModal + ' button.close').click(function () {
+//             $(theModal + ' iframe').attr('src', videoSRC);
+//         });
+//     });
+//}
+
 $(document).ready(main);
