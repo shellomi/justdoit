9dd1274d2d6eb7d92103f4e21b6b462371deaa0e
binson1989 <binson1989@gmail.com>
2016-01-03 12:12:14 +0530

    Extended templates to avoid duplicate html #1

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'ecommerce/index.html')
+
+
+def stationery(request):
+    return render(request, 'ecommerce/stationery.html')
